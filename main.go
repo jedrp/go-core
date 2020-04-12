260112bab024bf87630d7712a0ada8108a14bba4
Jed <thanhhoa.a1@gmail.com>
2020-04-12 14:02:07 +0700

    upgrade go version

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jedrp/go-core/boo"
-	"github.com/jedrp/go-core/booo"
-)
-
 func main() {
-	fmt.Println(boo.AAAA.String())
-	fmt.Println(boo.AAAA.String())
-	fmt.Println(boo.AAAA.String())
-	booo.PrintValue()
 }
